216a40421c3cc9f4e92e04fd67a58c2206e53219
greggo58 <greggo58@gmail.com>
2022-02-27 14:26:35 +0000

    Add home views

---
+++ home/views.py
@@ -0,0 +1,10 @@
+""" Import render """
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    """ View to return index page """
+
+    return render(request, 'home/index.html')
