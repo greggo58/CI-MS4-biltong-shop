7399ea17c1dba9cca9135d1061c2699a14311a16
greggo58 <greggo58@gmail.com>
2022-02-27 19:47:21 +0000

    Fixing rendering bug with css

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
+""" Import render """
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
+def all_products(request):
+    """ View to return products page, including sorting and search queries """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
