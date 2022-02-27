3771e683dff055295f8615f3c3267f62730e1906
greggo58 <greggo58@gmail.com>
2022-02-27 19:30:34 +0000

    Admin and models added

--- products/models.py
+++ products/models.py
@@ -5,12 +5,15 @@ from django.db import models
 
 class Category(models.Model):
     """ Class for categories """
+    class Meta:
+        verbose_name_plural = "Categories"
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
 
+    """ Friendly name method """
     def get_friendly_name(self):
         return self.friendly_name
 
