88202e7dc4e925ed652b9bdb950145c9bbda4e2f
greggo58 <greggo58@gmail.com>
2022-03-26 09:37:20 +0000

    Added context variable with delivery calc

--- biltong_shop/settings.py
+++ biltong_shop/settings.py
@@ -74,9 +74,13 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', # REQUIRED BY ALLAUTH - DO NOT REMOVE
+
+                # REQUIRED BY ALLAUTH - DO NOT REMOVE
+                'django.template.context_processors.request',
+
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents'
             ],
         },
     },
@@ -154,6 +158,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'), )
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
