1bd99db5fcab254841ee8c8293a745537525a132
greggo58 <greggo58@gmail.com>
2022-03-29 15:40:50 +0000

    Deploy AWS static and media files to heroku

---
+++ custom_storages.py
@@ -0,0 +1,13 @@
+""" Custom storage """
+from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+
+
+class StaticStorage(S3Boto3Storage):
+    """ StaticStorage class """
+    location = settings.STATICFILES_LOCATION
+
+
+class MediaStorage(S3Boto3Storage):
+    """ MediaStorage class """
+    location = settings.MEDIAFILES_LOCATION
