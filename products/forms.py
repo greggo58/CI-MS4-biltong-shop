395fe8c448a0d3964aa16bca0ae2237395da4f93
greggo58 <greggo58@gmail.com>
2022-03-30 08:49:38 +0000

    Fix custom clearable image

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,5 @@
 """ Products forms """
 from django import forms
-from .widgets import CustomClearableFileInput
 from .models import Product, Category
 
 
@@ -10,9 +9,6 @@ class ProductForm(forms.ModelForm):
         """ Meta Class """
         model = Product
         fields = '__all__'
-
-    image = forms.ImageField(label='Image',
-                             required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
