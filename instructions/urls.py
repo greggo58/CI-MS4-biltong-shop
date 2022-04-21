04e9c0117a7c384d232e4f68bab49813048e7f68
greggo58 <greggo58@gmail.com>
2022-04-21 14:01:43 +0000

    Add edit and delete functionality to instructions

--- instructions/urls.py
+++ instructions/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.instructions, name='instructions'),
     path('add/', views.add_recipe, name='add_recipe'),
+    path('edit/<int:recipe_id>/', views.edit_recipe, name='edit_recipe'),
+    path('delete/<int:recipe_id>/',
+         views.delete_recipe, name='delete_recipe'),
 ]
--- instructions/views.py
+++ instructions/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import (render, redirect, reverse,
                               get_object_or_404, get_list_or_404)
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
-from django.db.models.functions import Lower
 from .models import Recipe
 from .forms import RecipeForm
 
@@ -14,9 +12,9 @@ def instructions(request):
     """ A view to return the instructions page """
 
     recipes = Recipe.objects.all()
-    
+
     template = 'instructions/instructions.html'
-    
+
     context = {
         'recipes': recipes
     }
@@ -49,3 +47,46 @@ def add_recipe(request):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def edit_recipe(request, recipe_id):
+    """ Edit a recipe in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES, instance=recipe)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated recipe!')
+            return redirect(reverse('instructions'))
+        else:
+            messages.error(request, 'Failed to update recipe. \
+                Please ensure the form is valid.')
+    else:
+        form = RecipeForm(instance=recipe)
+        messages.info(request, f'You are editing {recipe.name}')
+
+    template = 'instructions/edit_recipe.html'
+    context = {
+        'form': form,
+        'recipe': recipe,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_recipe(request, recipe_id):
+    """ Delete a recipe from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    recipe.delete()
+    messages.success(request, 'Recipe deleted!')
+    return redirect(reverse('instructions'))
