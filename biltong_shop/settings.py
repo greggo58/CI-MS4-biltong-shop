708f8455c343881806f31acbfd7aa5b554a22ea2
greggo58 <79915199+greggo58@users.noreply.github.com>
2022-11-18 18:24:05 +0000

    Update settings.py

--- biltong_shop/settings.py
+++ biltong_shop/settings.py
@@ -181,10 +181,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, STATIC_URL), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'), )
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = (os.path.join(BASE_DIR, MEDIA_URL), )
+MEDIA_ROOT = (os.path.join(BASE_DIR, 'media/'), )
 
 if 'USE_AWS' in os.environ:
     # Cache control
